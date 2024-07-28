f9c04c10240e0f4daaba23f58b780ed612bfdbba
Zainabilal123 <bilalzainab121@gmail.com>
2024-07-28 13:56:38 +0500

    added files

---
+++ script.js
@@ -0,0 +1,34 @@
+// problem #1
+// var value = "123abc";
+// var actualvalue =parseInt(value);
+// console.log(actualvalue);
+
+// problem #2
+// var num = "45.67xyz";
+// var actualnumber = parseFloat(num);
+// console.log(actualnumber);
+
+// problem #3
+// var stringvalue = "123.45";
+// var convertnumber = Number(stringvalue);
+// console.log(convertnumber);
+
+// problem #4
+// var value = 789;
+// var convertedString = value.toString();
+// console.log(convertedString);
+
+// problem #5
+// var number = "256px";
+//     var convertednumber = parseInt(number);
+
+   
+// console.log(convertednumber);
+//  var convertedstring = number.toString();
+
+ 
+
+// console.log(convertedstring);
+
+
+
